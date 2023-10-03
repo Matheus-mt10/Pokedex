a5d775bf56af81bcdc953417249c7f2f6d94c50a
matheusDev <matheus.info29@gmail.com>
2023-10-03 02:45:50 -0300

    Updade -  Processando a requisição

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,23 @@
 const offset = 0;
 const limit = 10;
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+.then(function (response) {
+    response.json();
+}
+.then(function (jsonBody){
+    console.log(jsonBody);
+})
+
+.catch(function (error) {
+    console.log(error)
+}
+.finally(function () {
+        console.log("Requisição concluída")
+})
+)
+
+)
+
