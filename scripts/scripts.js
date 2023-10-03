8c370ca6c9972a2aae5e25fbeec5dde46d8e15e7
matheusDev <matheus.info29@gmail.com>
2023-10-03 13:44:17 -0300

    Update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,34 @@
 const offset = 0;
 const limit = 10;
-
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+function converPokemonToLi(pokemon) {
+    return `
+
+        <li class="pokemon">
+            <span class="number"><strong>#001</strong></span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+
+                <ul class="types">
+                    <li class="type">Grass</li>
+                    <li class="type">Poison</li>
+                </ul>
+
+                <img src="images/bulbaR.gif" alt="${pokemon.name}">
+
+            </div>
+               
+        </li>
+    `
+}
+
+
+const pokemonList = window.document.getElementById('pokemonList');
+
+
+
 
 fetch(url)
 .then(function (response) {
@@ -10,14 +37,17 @@ fetch(url)
 .then(function (jsonBody){
     console.log(jsonBody);
 })
+.then(function(pokemons) {
+    
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += converPokemonToLi(pokemon);
+    }
 
+})
 .catch(function (error) {
     console.log(error)
-}
-.finally(function () {
-        console.log("Requisição concluída")
 })
-)
 
 )
 
