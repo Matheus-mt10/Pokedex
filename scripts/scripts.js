efeabf752f57d296cc887a585fbaba2c56b64ba6
matheusDev <matheus.info29@gmail.com>
2023-10-04 03:58:12 -0300

    Update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,52 +2,49 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-function converPokemonToLi(pokemon) {
-    return `
 
-        <li class="pokemon">
+
+function convertPokemonToLi(pokemon) {
+    return `
+            <li class="pokemon">
             <span class="number"><strong>#001</strong></span>
             <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
+        <div class="detail">
 
-                <ul class="types">
-                    <li class="type">Grass</li>
-                    <li class="type">Poison</li>
-                </ul>
+            <ul class="types">
+                <li class="type">Grass</li>
+                <li class="type">Poison</li>
+            </ul>
 
-                <img src="images/bulbaR.gif" alt="${pokemon.name}">
+            img src="images/bulbaR.gif" alt="${pokemon.name}">
 
-            </div>
-               
+        </div>
+   
         </li>
+    
     `
 }
-
 
 const pokemonList = window.document.getElementById('pokemonList');
 
 
-
-
 fetch(url)
-.then(function (response) {
-    response.json();
-}
-.then(function (jsonBody){
-    console.log(jsonBody);
-})
-.then(function(pokemons) {
+
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            console.log(convertPokemonToLi(pokemon))
+            pokemonList.innerHTML+= convertPokemonToLi(pokemon)
+            
+        }
+        
     
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        pokemonList.innerHTML += converPokemonToLi(pokemon);
-    }
+    })
+    .catch((error) => console.log(error))
 
-})
-.catch(function (error) {
-    console.log(error)
-})
 
-)
 
